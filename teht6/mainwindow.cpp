00cf25faca6fcc94b21a76b78dac23d2359df269
t3roja <t3roja@students.oamk.fi>
2024-02-24 13:11:25 +0200

    teht6 aloitettu

---
+++ teht6/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_btnCount_clicked()
+{
+    qDebug()<<"Count button pressed";
+    x++;
+    QString s = QString::number(x);
+    ui->txtCount->setText(s);
+}
+
+
+void MainWindow::on_btnReset_clicked()
+{
+    qDebug()<<"Reset button pressed";
+    x=0;
+    QString s = QString::number(x);
+    ui->txtCount->setText(s);
+
+}
+
